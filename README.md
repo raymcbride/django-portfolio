9e955a18f167789f182a28cd2a58238da15b1ce9
Ray <ray@raymc>
2015-05-26 15:07:39 +0100

    tox configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #django-portfolio
+[![Coverage
+Status](https://coveralls.io/repos/raymcbride/django-portfolio/badge.svg)](https://coveralls.io/r/raymcbride/django-portfolio)
--- test_project/urls.py
+++ test_project/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^portfolio/', include('porfolio.urls')),
+    url(r'^portfolio/', include('portfolio.urls')),
 )
